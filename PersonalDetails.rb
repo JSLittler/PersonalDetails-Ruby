4b444765bf48278f19efe2751fb1d6c21ae80b79
John Littler <JSLittler@icloud.com>
2018-06-27 21:22:58 +0100

    Personal details sentence compiler

---
+++ PersonalDetails.rb
@@ -0,0 +1,9 @@
+print "What is your first name?"
+first_name = gets.chomp
+print "what is your second name?"
+second_name = gets.chomp
+print "How old are you?"
+age = gets.chomp
+
+print "your name is #{first_name} #{second_name},"
+print "and you are #{age} years old."
